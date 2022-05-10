870c9dac8a630219741d8bfdf8e6e262993026e8
viswa_amit <viswaamitcodes@outlook.com>
2022-05-10 21:48:18 +0530

    Update esri-map.js

--- js/esri-map.js
+++ js/esri-map.js
@@ -20,7 +20,7 @@ WildRydes.map = WildRydes.map || {};
     ) {
         var wrMap = WildRydes.map;
 
-        var map = new Map({ basemap: 'gray-vector' });
+        var map = new Map({ basemap: 'streets-navigation-vector' });
 
         var view = new MapView({
             center: [-122.31, 47.60],
